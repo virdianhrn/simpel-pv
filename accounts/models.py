5670b81c93e38cd4b6a246c1693d02f55ae03e4f
Virdian Harun Prayoga <virdian.harun@ui.ac.id>
2025-06-03 09:51:23 +0700

    Alter foto upload path

--- accounts/models.py
+++ accounts/models.py
@@ -21,7 +21,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=2, choices=USER_ROLES)
     jabatan = models.CharField(max_length=255, blank=True)
-    foto = models.ImageField(upload_to='foto_user/', blank=True)
+    foto = models.ImageField(upload_to='media/foto_user/', blank=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - {self.role}"
