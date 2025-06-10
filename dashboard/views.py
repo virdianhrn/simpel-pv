260a6bb58a6bae8d83c46a2e13caa20a7355188e
Virdian Harun Prayoga <virdian.harun@ui.ac.id>
2025-06-10 14:28:32 +0700

    Create simple dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from pelatihan.models import Pelatihan
 
-# Create your views here.
+def dashboard(request):
+    semua_pelatihan = Pelatihan.objects.all()
+    
+    context = {
+        'daftar_pelatihan': semua_pelatihan,
+    }
+    
+    # Merender template HTML dengan konteks yang diberikan
+    return render(request, 'dashboard.html', context)
