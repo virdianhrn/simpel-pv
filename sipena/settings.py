2a68cc50b55e91cdac25fcbcdf6a4ba3a4ae7689
Virdian Harun Prayoga <virdian.harun@ui.ac.id>
2025-06-10 13:44:21 +0700

    Dashboard app prep

--- sipena/settings.py
+++ sipena/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'pelatihan'
+    'pelatihan',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
--- sipena/urls.py
+++ sipena/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("pelatihan/", include("pelatihan.urls")),
+    path("dashboard/", include("dashboard.urls")),
 ]
 
 if settings.DEBUG:
