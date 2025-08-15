1568e8485c074f9f4f0633fdf2b7a0bb8d158e70
Virdian Harun Prayoga <virdianh@gmail.com>
2025-08-15 14:53:06 +0700

    Fix setting for Synology NAS

--- sipena/settings.py
+++ sipena/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
  
 DEBUG = bool(os.environ.get("DEBUG", default=0))
  
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","10.10.2.42").split(",")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
