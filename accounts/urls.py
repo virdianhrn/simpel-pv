344e26b5d7a59d7bb29f14e196bdaa7b72fe62ab
Virdian Harun Prayoga <virdian.harun@ui.ac.id>
2025-09-01 11:55:31 +0700

    Create simple user profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('', views.profile_view, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
 
-# Create your views here.
+def profile_view(request):
+    profile = get_object_or_404(Profile, user=request.user)
+    context = {
+        'profile': profile,
+        'user': request.user
+    }
+    return render(request, 'profile.html', context)
