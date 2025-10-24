e2e58102d025b2f514be1837aaf78ebe6e32a4fe
Virdian Harun Prayoga <virdianh@gmail.com>
2025-10-24 19:31:43 +0700

    Fix old url matching template

--- pelatihan/urls.py
+++ pelatihan/urls.py
@@ -5,10 +5,10 @@ app_name = 'pelatihan'
 
 urlpatterns = [
     path('add', views.add, name='add'),
-    path("<uuid:pelatihan_id>", views.DetailView.as_view(), name="detail"),
-    path("<uuid:pelatihan_id>/edit", views.edit, name="edit"),
-    path("<uuid:pelatihan_id>/delete", views.delete, name="delete"),
-    path("<uuid:pelatihan_id>/skip/<uuid:document_id>", views.skip_document, name="skip_document"),
-    path("<uuid:pelatihan_id>/download", views.download_merged_docs, name="download_merged_docs"),
-    path("<uuid:pelatihan_id>/verifikasi/<uuid:document_id>", views.verifikasi_dokumen, name="verifikasi_dokumen"),
+    path("<str:pelatihan_id>/edit", views.edit, name="edit"),
+    path("<str:pelatihan_id>/delete", views.delete, name="delete"),
+    path("<str:pelatihan_id>/download", views.download_merged_docs, name="download_merged_docs"),
+    path("<str:pelatihan_id>/skip/<str:document_id>", views.skip_document, name="skip_document"),
+    path("<str:pelatihan_id>/verifikasi/<str:document_id>", views.verifikasi_dokumen, name="verifikasi_dokumen"),
+    path("<str:pelatihan_id>", views.DetailView.as_view(), name="detail"),
 ]
