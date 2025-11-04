a7c0c2c194a1d2cfba34cf75dab7ffe47119a281
Virdian Harun Prayoga <virdianh@gmail.com>
2025-11-04 14:07:53 +0700

    Fix unique together

--- pelatihan/models.py
+++ pelatihan/models.py
@@ -246,7 +246,6 @@ class PelatihanInstruktur(models.Model):
     class Meta:
         verbose_name = "Instruktur Pelatihan"
         verbose_name_plural = "Daftar Instruktur Pelatihan"
-        unique_together = ('pelatihan', 'instruktur', 'materi')
 
     def __str__(self):
         return f"{self.instruktur.nama} - {self.pelatihan.judul}"
